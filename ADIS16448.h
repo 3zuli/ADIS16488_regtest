040304137dad92dd588a2399ab733c45f8467057
3zuli <33zuli@gmail.com>
2017-09-03 20:36:51 +0200

    Initial commit

---
+++ ADIS16448.h
@@ -0,0 +1,164 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  September 2016
+//  Author: Juan Jose Chong <juan.chong@analog.com>
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  ADIS16448.h
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 
+//  This library provides all the functions necessary to interface the ADIS16448 IMU with a 
+//  PJRC 32-Bit Teensy 3.2 Development Board. Functions for SPI configuration, reads and writes,
+//  and scaling are included. This library may be used for the entire ADIS1646X family of devices 
+//  with some modification.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining
+//  a copy of this software and associated documentation files (the
+//  "Software"), to deal in the Software without restriction, including
+//  without limitation the rights to use, copy, modify, merge, publish,
+//  distribute, sublicense, and/or sell copies of the Software, and to
+//  permit persons to whom the Software is furnished to do so, subject to
+//  the following conditions:
+//
+//  The above copyright notice and this permission notice shall be
+//  included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+//  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+//  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+//  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef ADIS16448_h
+#define ADIS16448_h
+#include "Arduino.h"
+#include <SPI.h>
+
+#define ADIS16480_PAGE_SIZE 0x80
+#define ADIS16480_REG(page, reg) ((page) * ADIS16480_PAGE_SIZE + (reg))
+
+// User Register Memory Map from Table 6
+#define PAGE_ID ADIS16480_REG(0x00,0x00)  //Flash memory write count
+#define TEMP_OUT ADIS16480_REG(0x00,0x0E)  //Flash memory write count
+#define XGYRO_OUT ADIS16480_REG(0x00,0x12)  //X-axis gyroscope output
+#define YGYRO_OUT ADIS16480_REG(0x00,0x16)  //Y-axis gyroscope output
+#define ZGYRO_OUT ADIS16480_REG(0x00,0x1A)  //Z-axis gyroscope output
+#define XGYRO_DELTAANG_OUT ADIS16480_REG(0x00,0x42)  //X-axis DELTAANG gyroscope output
+#define YGYRO_DELTAANG_OUT ADIS16480_REG(0x00,0x46)  //Y-axis DELTAANG gyroscope output
+#define ZGYRO_DELTAANG_OUT ADIS16480_REG(0x00,0x4A)  //Z-axis DELTAANG gyroscope output
+#define XACCL_OUT ADIS16480_REG(0x00,0x1E)  //X-axis accelerometer output
+#define YACCL_OUT ADIS16480_REG(0x00,0x22)  //Y-axis accelerometer output
+#define ZACCL_OUT ADIS16480_REG(0x00,0x26)  //Z-axis accelerometer output
+#define XMAGN_OUT ADIS16480_REG(0x00,0x28)  //X-axis magnetometer output
+#define YMAGN_OUT ADIS16480_REG(0x00,0x2A)  //Y-axis magnetometer output
+#define ZMAGN_OUT ADIS16480_REG(0x00,0x2C)  //Z-axis magnetometer output
+#define BARO_OUT ADIS16480_REG(0x00,0x30)   //Barometer pressure measurement, high word
+#define TEMP_OUT ADIS16480_REG(0x00,0x0E)   //Temperature output
+#define XGYRO_OFF ADIS16480_REG(0x02,0x12)  //X-axis gyroscope bias offset factor
+#define YGYRO_OFF ADIS16480_REG(0x02,0x16)  //Y-axis gyroscope bias offset factor
+#define ZGYRO_OFF ADIS16480_REG(0x02,0x1A)  //Z-axis gyroscope bias offset factor
+#define XACCL_OFF ADIS16480_REG(0x02,0x1E)  //X-axis acceleration bias offset factor
+#define YACCL_OFF ADIS16480_REG(0x02,0x22)  //Y-axis acceleration bias offset factor
+#define ZACCL_OFF ADIS16480_REG(0x02,0x26)  //Z-axis acceleration bias offset factor
+// // // // // 
+#define XMAGN_HIC ADIS16480_REG(0x02,0x28)  //X-axis magnetometer, hard iron factor
+#define YMAGN_HIC ADIS16480_REG(0x02,0x2A)  //Y-axis magnetometer, hard iron factor
+#define ZMAGN_HIC ADIS16480_REG(0x02,0x2C)  //Z-axis magnetometer, hard iron factor
+#define XMAGN_SIC ADIS16480_REG(0x02,0x2E)  //X-axis magnetometer, soft iron factor
+#define YMAGN_SIC ADIS16480_REG(0x02,0x30)  //Y-axis magnetometer, soft iron factor
+#define ZMAGN_SIC ADIS16480_REG(0x02,0x32)  //Z-axis magnetometer, soft iron factor
+#define GPIO_CTRL ADIS16480_REG(0x03,0x08)  //GPIO control
+#define MSC_CTRL ADIS16480_REG(0x03,0x06)   //MISC control // FNCTIO_CTRL
+#define DEC_RATE ADIS16480_REG(0x03,0x0C)   //Sample clock/Decimation filter control //DEC_RATE
+#define NULL_CNFG ADIS16480_REG(0x03,0x0E)  //Bias null command
+//#define SENS_AVG 0x38   //Digital filter control
+#define SEQ_CNT 0x3A    //MAGN_OUT and BARO_OUT counter
+#define DIAG_STAT ADIS16480_REG(0x00,0x0A)  //System status
+#define GLOB_CMD 0x3E   //System command
+#define ALM_MAG1 0x40   //Alarm 1 amplitude threshold
+#define ALM_MAG2 0x42   //Alarm 2 amplitude threshold
+#define ALM_SMPL1 0x44  //Alarm 1 sample size
+#define ALM_SMPL2 0x46  //Alarm 2 sample size
+#define ALM_CTRL 0x48   //Alarm control
+#define LOT_ID1 0x52    //Lot identification number
+#define LOT_ID2 0x54    //Lot identification number
+#define PROD_ID ADIS16480_REG(0x00,0x7E)    //Product identifier
+#define SERIAL_NUM ADIS16480_REG(0x04,0x20) //Lot-specific serial number
+
+// ADIS16448 class definition
+class ADIS16448 {
+
+public:
+  // Constructor with configurable CS, data ready, and HW reset pins
+
+  // ADIS16448(int CS, int DR, int RST, int MOSI, int MISO, int CLK);
+  ADIS16448(int CS, int DR, int RST);
+
+  // Destructor
+  ~ADIS16448();
+
+  struct ImuDataRaw {
+    int32_t gx, gy, gz;
+    int32_t gdx, gdy, gdz;    
+    int32_t ax, ay, az; 
+    int32_t mx, my, mz; 
+  };
+
+  struct ImuData {
+    float gx, gy, gz;
+    float gdx, gdy, gdz;    
+    float ax, ay, az; 
+    float mx, my, mz;   
+  };
+
+  // Performs hardware reset by sending pin 8 low on the DUT for 2 seconds
+  int resetDUT(uint8_t ms);
+
+  // Sets SPI bit order, clock divider, and data mode
+  int configSPI();
+
+  // Read single register from sensor
+  int16_t regRead(uint8_t regAddr);
+
+  // Read OUT register and its corresponding LOW register from sensor
+  int32_t regRead32(uint8_t regAddr);
+
+  // Write register
+  int regWrite(uint8_t regAddr, int16_t regData);
+
+  // Read sensor data using a burst read
+  int16_t *burstRead(void);
+
+  // Sequentially read all sensor data
+  int16_t *readAll(void);
+
+  // Scale accelerator data
+  float accelScale(int16_t sensorData);
+
+  // Scale gyro data
+  float gyroScale(int16_t sensorData);
+
+  // Scale gyro DELTAANG data
+  float gyroDeltaScale(int16_t sensorData);
+
+  // Scale temperature data
+  float tempScale(int16_t sensorData);
+
+ //Scale barometer data. Returns scaled data as float.
+  float pressureScale(int16_t sensorData);
+
+  //Scale magnetometer data. Returns scaled data as float.
+  float magnetometerScale(int16_t sensorData);
+
+private:
+  // Variables to store hardware pin assignments
+  int _CS;
+  int _DR;
+  int _RST;
+  int _stall = 1; //20
+
+};
+
+#endif
---
+++ ADIS16488_regtest.ino
@@ -0,0 +1,325 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  September 2016
+//  Author: Juan Jose Chong <juan.chong@analog.com>
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  ADIS16448_Teensy_BurstRead_Example.ino
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 
+//  This Arduino project interfaces with an ADIS16448 using SPI and the 
+//  accompanying C++ libraries, reads IMU data in LSBs, scales the data, and 
+//  outputs measurements to a serial debug terminal (PuTTY) via the onboard 
+//  USB serial port.
+//
+//  This project has been tested on a PJRC 32-Bit Teensy 3.2 Development Board, 
+//  but should be compatible with any other embedded platform with some modification.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining
+//  a copy of this software and associated documentation files (the
+//  "Software"), to deal in the Software without restriction, including
+//  without limitation the rights to use, copy, modify, merge, publish,
+//  distribute, sublicense, and/or sell copies of the Software, and to
+//  permit persons to whom the Software is furnished to do so, subject to
+//  the following conditions:
+//
+//  The above copyright notice and this permission notice shall be
+//  included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+//  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+//  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+//  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//  Pinout for a Teensy 3.2 Development Board
+//  RST = D6
+//  SCK = D13/SCK
+//  CS = D10/CS
+//  DOUT(MISO) = D12/MISO
+//  DIN(MOSI) = D11/MOSI
+//  DR = D2
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include "ADIS16448.h"
+#include <SPI.h>
+#include <cmath>
+
+// Uncomment to enable debug
+//#define DEBUG
+
+const int dbgpin = 3;
+
+const float gdelta_scale = 0.022; // 720.0/(2^15);
+const float gscale = 450.0/pow(2,31);
+const float imu_dt = 1.0/(2460/4);
+
+// Initialize Variables
+// Temporary Data Array
+volatile int16_t *burstData;
+
+// Accelerometer
+float AXS, AYS, AZS = 0;
+
+// Gyro
+float GXS, GYS, GZS = 0;
+volatile float GINTX, GINTY, GINTZ=0;
+volatile float gdxraw, gdyraw, gdzraw=0;
+volatile float GDELTAXS, GDELTAYS, GDELTAZS = 0;
+
+// Magnetometer
+float MXS, MYS, MZS = 0;
+
+// Barometer
+float BAROS = 0;
+
+// Control registers
+int MSC = 0;
+int SENS = 0;
+int SMPL = 0;
+
+// Temperature
+float TEMPS = 0;
+
+// Delay counter variable
+int printCounter = 0;
+
+// Call ADIS16448 Class
+ADIS16448 IMU(10,2,6); // Chip Select, Data Ready, Reset Pin Assignments
+
+uint32_t t_start, t_prev;
+
+void setup()
+{
+    Serial.begin(115200); // Initialize serial output via USB
+    pinMode(dbgpin, OUTPUT);
+    IMU.configSPI(); // Configure SPI communication
+    delay(1000); // Give the part time to start up
+//    IMU.regWrite(MSC_CTRL, 0x16);  // Enable Data Ready, set polarity
+//    delay(20); 
+    //IMU.regWrite(SENS_AVG, 0x402); // Set digital filter
+    //delay(20);
+//    IMU.regWrite(SMPL_PRD, 0x01), // Disable decimation
+//    delay(20);
+
+    // Set decimation rate to 4 -> 616Hz
+    IMU.regWrite(PAGE_ID, 3); // turn to page 3
+    IMU.regWrite(DEC_RATE, 0x03); // set DEC_RATE to 3 -> decimation = 3+1=4
+    delay(20);
+//    Serial.println(IMU.regRead(0x0E),HEX);
+//    delay(2000);
+    IMU.regWrite(PAGE_ID, 0); // turn to page 0 to continue normal operation
+    delay(20);
+
+    // Read the control registers once to print to screen
+    //MSC = IMU.regRead(MSC_CTRL);
+//    SENS = IMU.regRead(SENS_AVG);
+    //SMPL = IMU.regRead(SMPL_PRD);
+
+    t_start = millis();
+    t_prev=t_start;
+    attachInterrupt(2, grabData, RISING); // Attach interrupt to pin 2. Trigger on the rising edge
+//    int i=0;
+//    while(1){
+//      delay(100);
+////      IMU.regWrite(PAGE_ID,0);
+////      Serial.print(String(i++)+": "+IMU.regRead(PAGE_ID)+" ");
+////      delay(1);
+////      IMU.regWrite(PAGE_ID,3);
+////      Serial.println(IMU.regRead(PAGE_ID));
+////      Serial.println(IMU.regRead32(XGYRO_OUT)*gscale);
+//      //Serial.println(String(i++)+": "+IMU.regRead(PROD_ID)+" "+IMU.regRead(XGYRO_OUT));
+//    }
+}
+
+volatile uint32_t grabcnt = 0;
+// Function used to read register values when an ISR is triggered using the IMU's DataReady output
+void grabData()
+{
+    detachInterrupt(2);
+    digitalWrite(dbgpin,HIGH);
+    grabcnt++;
+    IMU.configSPI(); // Configure SPI before the read. Useful when talking to multiple SPI devices
+//    burstData = IMU.burstRead(); // Read data and insert into array
+    burstData = IMU.readAll();
+    digitalWrite(dbgpin,LOW);
+    digitalWrite(dbgpin,HIGH);
+//    gdxraw += *(burstData + 12);
+//    gdyraw += *(burstData + 13);
+//    gdzraw += *(burstData + 14);
+    GINTX += IMU.gyroScale(*(burstData + 1))*imu_dt; //Scale X Gyro
+    GINTY += IMU.gyroScale(*(burstData + 2))*imu_dt; //Scale Y Gyro
+    GINTZ += IMU.gyroScale(*(burstData + 3))*imu_dt; //Scale Z Gyro
+    GDELTAXS += *(burstData + 12)*gdelta_scale;
+    GDELTAYS += *(burstData + 13)*gdelta_scale;
+    GDELTAZS += *(burstData + 14)*gdelta_scale;
+    digitalWrite(dbgpin,LOW);
+    attachInterrupt(2,grabData,RISING);
+}
+
+// Function used to scale all acquired data (scaling functions are included in ADIS16448.cpp)
+void scaleData()
+{
+    GXS = IMU.gyroScale(*(burstData + 1)); //Scale X Gyro
+    GYS = IMU.gyroScale(*(burstData + 2)); //Scale Y Gyro
+    GZS = IMU.gyroScale(*(burstData + 3)); //Scale Z Gyro
+    AXS = IMU.accelScale(*(burstData + 4)); //Scale X Accel
+    AYS = IMU.accelScale(*(burstData + 5)); //Scale Y Accel
+    AZS = IMU.accelScale(*(burstData + 6)); //Scale Z Accel
+    MXS = IMU.magnetometerScale(*(burstData + 7)); //Scale X Mag
+    MYS = IMU.magnetometerScale(*(burstData + 8)); //Scale Y Mag
+    MZS = IMU.magnetometerScale(*(burstData + 9)); //Scale Z Mag
+    BAROS = IMU.pressureScale(*(burstData + 10)); //Scale Pressure Sensor
+    TEMPS = IMU.tempScale(*(burstData + 11)); //Scale Temp Sensor
+//    GDELTAXS = IMU.gyroDeltaScale(*(burstData + 12));
+//    GDELTAYS = IMU.gyroDeltaScale(*(burstData + 13));
+//    GDELTAZS = IMU.gyroDeltaScale(*(burstData + 14));  
+//    GDELTAXS = IMU.gyroDeltaScale(gdxraw);
+//    GDELTAYS = IMU.gyroDeltaScale(gdyraw);
+//    GDELTAZS = IMU.gyroDeltaScale(gdzraw);  
+}
+
+// Main loop. Print data to the serial port. Sensor sampling is performed in the ISR
+void loop()
+{   
+    
+    //uint32_t t_start = millis();
+    //IMU.configSPI(); // Configure SPI before the read. Useful when talking to multiple SPI devices
+    //burstData = IMU.burstRead(); // Read data and insert into array
+    //burstData = IMU.readAll();
+
+    detachInterrupt(2); //Detach interrupt to avoid overwriting data
+    uint32_t t_now = millis();
+    uint32_t dt = t_now-t_prev;
+    if(dt>500){
+        //Serial.println(grabcnt);
+//        Serial.println(1000*grabcnt/(float)dt);
+        grabcnt=0;
+        t_prev = t_now;
+    }
+    scaleData();
+//    Serial.print(GXS); Serial.print(",");
+//    Serial.print(GYS); Serial.print(",");
+//    Serial.print(GZS); Serial.print(",");
+
+//      Serial.print(gdxraw); Serial.print(",");
+//      Serial.print(gdyraw); Serial.print(",");
+//      Serial.print(gdzraw); Serial.print(",");
+//      Serial.print((*(burstData + 12))); Serial.print(",");
+//      Serial.print((*(burstData + 13))); Serial.print(",");
+//      Serial.print((*(burstData + 14))); Serial.print(",");
+    Serial.print(GDELTAXS); Serial.print(",");
+    Serial.print(GDELTAYS); Serial.print(",");
+    Serial.print(GDELTAZS); Serial.print(",");
+//    Serial.print(GINTX); Serial.print(",");
+//    Serial.print(GINTY); Serial.print(",");
+//    Serial.print(GINTZ); Serial.print(",");
+
+//    Serial.print(AXS); Serial.print(",");
+//    Serial.print(AYS); Serial.print(",");
+//    Serial.print(AZS); Serial.print(",");
+//    Serial.print(MXS); Serial.print(",");
+//    Serial.print(MYS); Serial.print(",");
+//    Serial.print(MZS); Serial.print(",");
+//    Serial.print(BAROS); Serial.print(",");
+////    Serial.print((*(burstData + 7))); Serial.print(","); // temperature test
+//    Serial.print(TEMPS); //Serial.print(",");
+    Serial.println();
+    attachInterrupt(2, grabData, RISING);
+    delay(50);
+    
+//    printCounter ++;
+//    if (printCounter >= 200000) // Delay for writing data to the serial port
+//    {
+        //detachInterrupt(2); //Detach interrupt to avoid overwriting data
+        /*scaleData(); // Scale data acquired from the IMU
+
+        //Clear the serial terminal and reset cursor
+        //Only works on supported serial terminal programs (Putty)
+        Serial.print("\033[2J");
+        Serial.print("\033[H");
+
+        // Print header
+        Serial.println(" ");
+        Serial.println("ADIS16448 Teensy Burst Read Example Program");
+        Serial.println("Juan Chong - September 2016");
+        Serial.println(" ");
+
+        // Print control registers to the serial port
+        Serial.println("Control Registers");
+        Serial.print("MSC_CTRL: ");
+        Serial.println(MSC,HEX);
+        Serial.print("SENS_AVG: ");
+        Serial.println(SENS,HEX);
+        Serial.print("SMPL_PRD: ");
+        Serial.println(SMPL,HEX);
+        Serial.println(" ");
+        Serial.println("Raw Output Registers");
+        
+        // Print scaled gyro data
+        Serial.print("XGYRO: ");
+        Serial.println(GXS);
+        Serial.print("YGYRO: ");
+        Serial.println(GYS);
+        Serial.print("ZGYRO: ");
+        Serial.println(GZS);
+      
+        // Print scaled accel data
+        Serial.print("XACCL: ");
+        Serial.println(AXS);
+        Serial.print("YACCL: ");
+        Serial.println(AYS);
+        Serial.print("ZACCL: ");
+        Serial.println(AZS);
+
+        // Print scaled magnetometer data
+        Serial.print("XMAG: ");
+        Serial.println(MXS);
+        Serial.print("YMAG: ");
+        Serial.println(MYS);
+        Serial.print("ZMAG: ");
+        Serial.println(MZS);
+
+        // Print scaled barometer data
+        Serial.print("BARO: ");
+        Serial.println(BAROS);
+
+        // Print scaled temp data
+        Serial.print("TEMP: ");
+        Serial.println(TEMPS);
+        Serial.println(" ");
+
+        Serial.println("Status Registers");
+
+        // Print Status Registers
+        Serial.print("DIAG_STAT: ");
+        Serial.println((*(burstData + 0)));
+        Serial.print("CHECKSUM: ");
+        Serial.println((*(burstData + 9)));
+ 
+#ifdef DEBUG 
+        // Print unscaled gyro data
+        Serial.print("XGYRO: ");
+        Serial.println((*(burstData + 1)));
+        Serial.print("YGYRO: ");
+        Serial.println((*(burstData + 2)));
+        Serial.print("ZGYRO: ");
+        Serial.println((*(burstData + 3)));
+      
+        // Print unscaled accel data
+        Serial.print("XACCL: ");
+        Serial.println((*(burstData + 4)));
+        Serial.print("YACCL: ");
+        Serial.println((*(burstData + 5)));
+        Serial.print("ZACCL: ");
+        Serial.println((*(burstData + 6)));
+        Serial.println(" ");
+       
+        // Print unscaled temp data
+        Serial.print("TEMP: ");
+        Serial.println((*(burstData + 7)));
+#endif
+        printCounter = 0;
+        //attachInterrupt(2, grabData, RISING);
+//    }*/
+}
